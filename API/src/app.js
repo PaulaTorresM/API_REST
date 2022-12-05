5bd3387d6a2ce0e161722cbfd6a785b40b3e4a0a
Your Name <you@example.com>
2022-12-05 18:15:53 -0500

    y otro :V

--- API/src/app.js
+++ API/src/app.js
@@ -21,7 +21,10 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 //Llamado de rutas
+app.use(express.static('public'));
 
+app.use('/public', express.static(__dirname + '/public'));
+app.use("/")
 app.use("/vehiculos",rutasVehiculos);
 app.use("/users",rutasUser);
 app.use("/ventas",rutasVentas);
